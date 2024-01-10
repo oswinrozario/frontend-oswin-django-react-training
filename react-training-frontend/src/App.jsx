70c3eb4cf02f6e7280f210686896c8c1fe814057
Oswin Rozario <oswinrozario@192.168.1.9>
2024-01-10 16:30:15 +0530

    implemented dashboard functionality, with private route

--- react-training-frontend/src/App.jsx
+++ react-training-frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./components/Login";
 import Start from "./components/Start";
 import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -12,6 +14,15 @@ function App() {
         <Route path="/" element={<Start />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
+
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
